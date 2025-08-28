9c9c37c4067f3f661da0fd807666424be2412a1e
Simón García <simon.garcial@udea.edu.co>
2025-08-28 10:05:21 -0500

    :sparkles: Get Reset Password Use Case

--- src/domain/models/one_time_token.go
+++ src/domain/models/one_time_token.go
@@ -5,7 +5,7 @@ import "time"
 type OneTimeTokenBase struct {
 	UserID  uint      `json:"user_id"`
 	Purpose string    `json:"purpose"`
-	Hash    string    `json:"hash"`
+	Hash    []byte    `json:"hash"`
 	IsUsed  bool      `json:"is_used"`
 	Expires time.Time `json:"expires"`
 }
