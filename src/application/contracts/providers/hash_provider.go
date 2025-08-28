52fbe0c33553deb391efff90b9fc4317ff75b283
Simón García <simon.garcial@udea.edu.co>
2025-08-27 22:48:47 -0500

    :sparkles: One Time Token on hash provider contract

--- src/application/contracts/providers/hash_provider.go
+++ src/application/contracts/providers/hash_provider.go
@@ -5,4 +5,6 @@ import application_errors "gormgoskeleton/src/application/shared/errors"
 type IHashProvider interface {
 	HashPassword(password string) (string, *application_errors.ApplicationError)
 	VerifyPassword(hashedPassword, password string) (bool, *application_errors.ApplicationError)
+	OneTimeToken() (string, []byte, *application_errors.ApplicationError)
+	ValidateOneTimeToken(hashedToken []byte, token string) bool
 }
