8f806ec544c9cef7a23cfdfe033f24a47dc54575
Simón García <simon.garcial@udea.edu.co>
2025-08-27 22:46:28 -0500

    :sparkles: HashProvider giving one time token

--- src/infrastructure/providers/hash_provider_test.go
+++ src/infrastructure/providers/hash_provider_test.go
@@ -56,3 +56,28 @@ func TestHashUniqueness(t *testing.T) {
 	assert.Nil(err2)
 	assert.NotEqual(hash1, hash2, "Hashes should be unique due to different salts")
 }
+
+func TestOneTimeToken(t *testing.T) {
+	assert := assert.New(t)
+
+	hashProvider := NewHashProvider()
+
+	token1, hash1, err1 := hashProvider.OneTimeToken()
+	token2, hash2, err2 := hashProvider.OneTimeToken()
+
+	assert.NotEmpty(token1)
+	assert.NotEmpty(token2)
+	assert.NotEmpty(hash1)
+	assert.NotEmpty(hash2)
+	assert.Nil(err1)
+	assert.Nil(err2)
+	assert.NotEqual(token1, token2, "One-time tokens should be unique")
+
+	// Validate tokens
+	valid1 := hashProvider.ValidateOneTimeToken(hash1, token1)
+	valid2 := hashProvider.ValidateOneTimeToken(hash2, token2)
+	invalid := hashProvider.ValidateOneTimeToken(hash1, token2)
+	assert.True(valid1, "Token 1 should be valid")
+	assert.True(valid2, "Token 2 should be valid")
+	assert.False(invalid, "Token 2 should not validate against hash 1")
+}
