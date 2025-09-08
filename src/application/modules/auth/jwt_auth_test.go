151df637d4e47eb4724684a51c668dbeb75eb4fe
Simón García <simon.garcial@udea.edu.co>
2025-09-08 10:29:41 -0500

    :white_check_mark: :shirt: postgres integration test

--- src/application/modules/auth/jwt_auth_test.go
+++ src/application/modules/auth/jwt_auth_test.go
@@ -8,7 +8,7 @@ import (
 	dtos "gormgoskeleton/src/application/shared/DTOs"
 	"gormgoskeleton/src/application/shared/locales"
 	"gormgoskeleton/src/application/shared/mocks"
-	dto_mocks "gormgoskeleton/src/application/shared/mocks/dtos"
+	dtomocks "gormgoskeleton/src/application/shared/mocks/dtos"
 	"gormgoskeleton/src/application/shared/status"
 	"gormgoskeleton/src/domain/models"
 
@@ -48,7 +48,7 @@ func TestAuthenticationUseCase(t *testing.T) {
 	testHashProvider.On("VerifyPassword", passwordBase.Hash, userCredentials.Password).Return(true, nil)
 	testJWTProvider.On("GenerateAccessToken", ctx, "1", mock.Anything).Return("accessToken", time.Now().Add(1*time.Hour), nil)
 	testJWTProvider.On("GenerateRefreshToken", ctx, "1").Return("refreshToken", time.Now().Add(24*time.Hour), nil)
-	testUserRepository.On("GetUserWithRole", uint(1)).Return(&dto_mocks.UserWithRole, nil)
+	testUserRepository.On("GetUserWithRole", uint(1)).Return(&dtomocks.UserWithRole, nil)
 	result := uc.Execute(ctx, locales.EN_US, userCredentials)
 	assert.NotNil(result)
 	assert.True(result.IsSuccess())
@@ -88,7 +88,7 @@ func TestAuthenticationUseCase_InvalidCredentials(t *testing.T) {
 		ID:           uint(1),
 	}, nil)
 	testHashProvider.On("VerifyPassword", passwordBase.Hash, userCredentials.Password).Return(false, nil)
-	testUserRepository.On("GetUserWithRole", uint(1)).Return(&dto_mocks.UserWithRole, nil)
+	testUserRepository.On("GetUserWithRole", uint(1)).Return(&dtomocks.UserWithRole, nil)
 	result := uc.Execute(ctx, locales.EN_US, userCredentials)
 	assert.NotNil(result)
 	assert.False(result.IsSuccess())
