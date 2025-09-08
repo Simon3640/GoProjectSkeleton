56470079d7c7a8064c7798dc6dd475babfe84a85
Simón García <simon.garcial@udea.edu.co>
2025-09-08 12:44:57 -0500

    :construction_worker: need a pathparam provider

--- src/infrastructure/api/routes/router.go
+++ src/infrastructure/api/routes/router.go
@@ -3,9 +3,17 @@ package routes
 import (
 	"gormgoskeleton/src/domain/models"
 	"gormgoskeleton/src/infrastructure/api/middlewares"
+	"gormgoskeleton/src/infrastructure/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+func wrapHandler(h http.HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		h(c.Writer, c.Request)
+	}
+}
 
 func Router(r *gin.RouterGroup) {
 	r.GET("/health-check", getHealthCheck)
@@ -13,22 +21,22 @@ func Router(r *gin.RouterGroup) {
 	private := r.Group("/")
 	private.Use(middlewares.AuthMiddleware())
 	// User routes
-	r.POST("/user", createUser)
-	private.GET("/user/:id", getUser)
-	private.PATCH("/user/:id", updateUser)
-	private.DELETE("/user/:id", deleteUser)
-	private.GET("/user", middlewares.QueryMidleWare[models.User](), getAllUser)
-	r.POST("/user-password", createUserAndPassword)
-	r.POST("/user/activate", activateUser)
+	r.POST("/user", wrapHandler(handlers.CreateUser))
+	private.GET("/user/:id", wrapHandler(handlers.GetUser))
+	private.PATCH("/user/:id", wrapHandler(handlers.UpdateUser))
+	private.DELETE("/user/:id", wrapHandler(handlers.DeleteUser))
+	private.GET("/user", middlewares.QueryMidleWare[models.User](), wrapHandler(handlers.GetAllUser))
+	r.POST("/user-password", wrapHandler(handlers.CreateUserAndPassword))
+	r.POST("/user/activate", wrapHandler(handlers.ActivateUser))
 
 	// Password routes
 	private.POST("/password", createPassword)
 	r.POST("/password/reset-token", createPasswordToken)
 
 	// Auth routes
-	r.POST("/auth/login", login)
-	r.POST("/auth/refresh", refreshAccessToken)
-	r.GET("/auth/password-reset/:identifier", requestPasswordReset)
-	r.GET("/auth/login-otp/:otp", loginOTP)
+	r.POST("/auth/login", wrapHandler(handlers.Login))
+	r.POST("/auth/refresh", wrapHandler(handlers.RefreshAccessToken))
+	r.GET("/auth/password-reset/:identifier", wrapHandler(handlers.RequestPasswordReset))
+	r.GET("/auth/login-otp/:otp", wrapHandler(handlers.LoginOTP))
 
 }
