fbcbec4e53e6835ae9f63d0098dc046d6528fa55
ruiseñor <ruiseor@113mail.com>
2025-12-11 10:00:37 -0500

    :bug: typo appllicationerrors -> applicationerros

--- src/application/modules/auth/mocks/mock_user_repository.go
+++ src/application/modules/auth/mocks/mock_user_repository.go
@@ -1,9 +1,9 @@
-// Package authmocks contains mock implementations of the contracts/auth/ interfaces
+// Package authmocks contains mock implementations of the modules/auth/contracts/ interfaces
 package authmocks
 
 import (
 	authcontracts "github.com/simon3640/goprojectskeleton/src/application/modules/auth/contracts"
-	appllicationerrors "github.com/simon3640/goprojectskeleton/src/application/shared/errors"
+	applicationerrors "github.com/simon3640/goprojectskeleton/src/application/shared/errors"
 	"github.com/simon3640/goprojectskeleton/src/domain/models"
 	"github.com/stretchr/testify/mock"
 )
@@ -16,21 +16,21 @@ type MockUserRepository struct {
 var _ authcontracts.IUserRepository = (*MockUserRepository)(nil)
 
 // GetUserWithRole gets a user with their role
-func (m *MockUserRepository) GetUserWithRole(id uint) (*models.UserWithRole, *appllicationerrors.ApplicationError) {
+func (m *MockUserRepository) GetUserWithRole(id uint) (*models.UserWithRole, *applicationerrors.ApplicationError) {
 	args := m.Called(id)
 	errorArg := args.Get(1)
 	if errorArg != nil {
-		return args.Get(0).(*models.UserWithRole), errorArg.(*appllicationerrors.ApplicationError)
+		return args.Get(0).(*models.UserWithRole), errorArg.(*applicationerrors.ApplicationError)
 	}
 	return args.Get(0).(*models.UserWithRole), nil
 }
 
 // GetByEmailOrPhone gets a user by email or phone
-func (m *MockUserRepository) GetByEmailOrPhone(emailOrPhone string) (*models.User, *appllicationerrors.ApplicationError) {
+func (m *MockUserRepository) GetByEmailOrPhone(emailOrPhone string) (*models.User, *applicationerrors.ApplicationError) {
 	args := m.Called(emailOrPhone)
 	errorArg := args.Get(1)
 	if errorArg != nil {
-		return nil, errorArg.(*appllicationerrors.ApplicationError)
+		return nil, errorArg.(*applicationerrors.ApplicationError)
 	}
 	return args.Get(0).(*models.User), nil
 }
