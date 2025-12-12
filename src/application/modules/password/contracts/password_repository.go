e0a93b56c28d091648018b13474454bd4ad2b28e
Simón García <simongarcia3640@gmail.com>
2025-12-11 19:30:46 -0500

    :lipstick: module imports standarization

--- src/application/modules/password/contracts/password_repository.go
+++ src/application/modules/password/contracts/password_repository.go
@@ -4,12 +4,12 @@ package passwordcontracts
 import (
 	contractsrepositories "github.com/simon3640/goprojectskeleton/src/application/contracts/repositories"
 	dtos "github.com/simon3640/goprojectskeleton/src/application/modules/password/dtos"
-	application_errors "github.com/simon3640/goprojectskeleton/src/application/shared/errors"
+	applicationerrors "github.com/simon3640/goprojectskeleton/src/application/shared/errors"
 	"github.com/simon3640/goprojectskeleton/src/domain/models"
 )
 
 // IPasswordRepository is the interface for the password repository
 type IPasswordRepository interface {
 	contractsrepositories.IRepositoryBase[dtos.PasswordCreate, dtos.PasswordUpdate, models.Password, models.PasswordInDB]
-	GetActivePassword(userEmail string) (*models.Password, *application_errors.ApplicationError)
+	GetActivePassword(userEmail string) (*models.Password, *applicationerrors.ApplicationError)
 }
